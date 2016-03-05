59ee34c9096a027cd433d4b5dd72d65ae0606eab
Rodney Winsor <patchvp@gmail.com>
2016-03-05 13:33:48 -0330

    refactored middleware into own file

--- server.js
+++ server.js
@@ -1,36 +1,15 @@
 var express = require('express');
+var middleware = require('./middleware.js');
 var app = express();
 var port = 3000;
 
-var middleware = {
-	requireAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		// new Date().toString()
-		console.log('request ' + new Date().toString() + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
 
 app.use(middleware.logger);
-
-//specify middleware up top
-//app.use(middleware.requireAuthentication);
-
 app.get('/about',middleware.requireAuthentication,function (req, res) {
 	res.send('Welcome to the page where we tell you all about us');
 });
 
 app.use(express.static(__dirname + '/public'));
-
-//sonsole.log(__dirname);
-
-
 app.listen(port, function() {
 	console.log('express server has started on port: ' + port);
-});//listen to port 3000
-
-
-//I'm adding a comment after adding git to my system
+});
