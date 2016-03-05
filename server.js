654ff0fb8d3259db78e17a9b7e4847418ac1dac0
Rodney Winsor <patchvp@gmail.com>
2016-03-05 16:38:44 -0330

    update port for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var middleware = require('./middleware.js');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 
 app.use(middleware.logger);
